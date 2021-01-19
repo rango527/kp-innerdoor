0d2773bdc5831226412cf024e8f57dbb2cb383f5
Patrick Allen <patrick.allen@ymedialabs.com>
2021-01-19 09:21:58 -0800

    review of provided benefits modal and minor fixes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+// KP component dependencies
+import { modalInit } from "modal-pattern/src/modal";
 import polyfillInit from "navigational-picker-pattern/src/polyfills";
 import navigationalPickerInit from "navigational-picker-pattern/src/navigational-picker";
+
+// KP component script
 document.onreadystatechange = function () {
   if (document.readyState === "complete") {
+    // Nav picker
     polyfillInit();
     navigationalPickerInit();
+
+    // Modal
+    modalInit();
   }
 };
