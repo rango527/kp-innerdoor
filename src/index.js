428f8ae669038117b05442053c81f9cc80da805f
Patrick Allen <patrick.allen@ymedialabs.com>
2021-01-20 12:25:17 -0800

    fix typo

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 // KP component dependencies
-import { modalInit } from "modal-pattern/src/modal";
-import polyfillInit from "navigational-picker-pattern/src/polyfills";
-import navigationalPickerInit from "navigational-picker-pattern/src/navigational-picker";
+import { modalInit } from 'modal-pattern/src/modal';
+import polyfillInit from 'navigational-picker-pattern/src/polyfills';
+import navigationalPickerInit from 'navigational-picker-pattern/src/navigational-picker';
 
 // KP component script
 document.onreadystatechange = function () {
-  if (document.readyState === "complete") {
+  if (document.readyState === 'complete') {
     // Nav picker
     polyfillInit();
     navigationalPickerInit();
