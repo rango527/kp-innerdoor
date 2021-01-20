0654d8b15a1514c7a5750efee210d7f8ab414c89
Patrick Allen <patrick.allen@ymedialabs.com>
2021-01-20 13:34:53 -0800

    fix care reminders local script issue

--- src/index.js
+++ src/index.js
@@ -14,3 +14,31 @@ document.onreadystatechange = function () {
     modalInit();
   }
 };
+
+// Generic click handler
+function addEvent(element, evnt, funct) {
+  if (element.attachEvent) return element.attachEvent('on' + evnt, funct);
+  else return element.addEventListener(evnt, funct, false);
+}
+
+// Care reminders - view more/less
+addEvent(document.getElementById('my-button'), 'click', function () {
+  let hiddenList = Array.from(document.getElementsByClassName('is-hidden'));
+  let visibleList = Array.from(document.getElementsByClassName('is-visible'));
+
+  if (hiddenList.length) {
+    document.querySelector('.care-reminders .view-toggle').innerHTML =
+      'View less care reminders';
+    hiddenList.forEach(function (el) {
+      el.classList.add('is-visible');
+      el.classList.remove('is-hidden');
+    });
+  } else {
+    document.querySelector('.care-reminders .view-toggle').innerHTML =
+      'View all care reminders';
+    visibleList.forEach(function (el) {
+      el.classList.add('is-hidden');
+      el.classList.remove('is-visible');
+    });
+  }
+});
